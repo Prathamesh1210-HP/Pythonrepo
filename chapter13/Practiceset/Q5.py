fd94c623aad7c96aebadf6db622fc768061ad402
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-08 13:50:04 +0530

    Q5 solved

---
+++ chapter13/Practiceset/Q5.py
@@ -0,0 +1,11 @@
+# Write a program to find the maximum of the numbers in a list using the reduce function.
+from functools import reduce
+
+l = [111, 2, 5, 5,555,]
+
+def greater(a, b):
+    if(a>b):
+        return a
+    return b
+
+print(reduce(greater, l))
