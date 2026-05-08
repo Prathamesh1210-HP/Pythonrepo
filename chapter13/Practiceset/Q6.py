a6eb527bbff9896208f3d8dda69cafd4c04e0193
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-08 13:58:03 +0530

    Q6 solved

---
+++ chapter13/Practiceset/Q6.py
@@ -0,0 +1,8 @@
+# Run pip freeze for the systemintrepreter. Take the contents and create a similar virtualenv.
+
+# It takes too much time, may crash the terminal
+# >>
+# pip freeze > requirenments.txt
+# virtualenv Patuenv
+# .\Patuenv\Scripts\activate.ps1
+# pip install -r .\requirenments.txt
