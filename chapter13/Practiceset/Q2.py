413898a3d00f65672a7de0e8e8c9338dc0a2529c
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-08 13:39:22 +0530

    Q2 solved

---
+++ chapter13/Practiceset/Q2.py
@@ -0,0 +1,11 @@
+# Write a program to input name, marks and phone number of a student and format it using the format function like below:
+
+# "The name of the student us Patu, his marks are 82 and phone number is 8899554466"
+
+name = input("Enter name: ")
+marks = int(input("Enter marks: "))
+phone = int(input("Enter phone number: "))
+
+s = "The name of the student is {}, his marks are {} and phone number is {}".format(name, marks, phone)
+
+print(s)
