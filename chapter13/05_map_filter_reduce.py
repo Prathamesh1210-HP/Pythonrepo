d0aa948c40917e446ffd433aa9db756f84ca458e
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-08 13:29:15 +0530

    map, filter, reduce used

---
+++ chapter13/05_map_filter_reduce.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+l = [1, 2, 3, 4, 5]
+
+square = lambda x: x*x
+
+sqlist = map(square, l)
+print(list(sqlist))
+
+
+# Filter example
+def even(n):
+    if (n%2 == 0):
+        return True
+    return False
+
+onlyEven = filter(even, l)
+print(list(onlyEven))
+
+# Reduce example
+def sum(a, b):
+    return a + b
+
+print(reduce(sum, l))
