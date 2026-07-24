378157b7355715c2434d5bd19a1a843e8718ea18
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-07-24 15:29:24 +0530

    question

--- chapter10/01_class.py
+++ chapter10/01_class.py
@@ -1,14 +1,23 @@
-class Car:
+class Account:
+    def __init__(self,bal,acc):
+        self.balance = bal
+        self.account_no = acc
 
-    def __init__(self) :
-        self.acc = False
-        self.brk = False
-        self.clutch = False
+    # debit method
+    def debit(self,amount): 
+        self.balance -= amount
+        print("Rs.",amount,"was debited", "Total balance = ", self.get_balance())
 
-    def start(self):
-        self.clutch = True
-        self.ac = True
-        print("Car started....")
+    # credit method
+    def credit(self,amount): 
+        self.balance += amount
+        print("Rs.",amount,"was credited", "Total balance = ", self.get_balance())
 
-car1 = Car()
-car1.start()
+    def get_balance(self):
+        return self.balance
+
+
+
+acc1 = Account(100000, 123456)
+acc1.debit(1000)
+acc1.credit(500)
