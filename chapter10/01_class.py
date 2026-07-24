98ae6f99f90f7edc168d57d26240cb781b9254c8
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-07-24 14:49:28 +0530

    abstraction

--- chapter10/01_class.py
+++ chapter10/01_class.py
@@ -1,23 +1,14 @@
-# Create student class that takes name and marks of 3 subjects as arguments in constructor. Then create a method to print the average.
+class Car:
 
-class Student:
-    print("...Student's result...");
+    def __init__(self) :
+        self.acc = False
+        self.brk = False
+        self.clutch = False
 
-    def __init__(self,name,marks):
-        self.name = name
-        self.marks = marks
+    def start(self):
+        self.clutch = True
+        self.ac = True
+        print("Car started....")
 
-    @staticmethod
-    def hello():
-        print("Hello")   
-
-    def getavg(self):
-        sum = 0
-        for val in self.marks:
-            sum += val
-        print("Hi", self.name, "Your avg score is: ", sum/3 )
-
-s1 = Student("Tony stark", [99,98,97])
-s1.getavg()
-s1.hello()
-# print(s1.name,m1,m2,m3)
+car1 = Car()
+car1.start()
