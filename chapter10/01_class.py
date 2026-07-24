7d673e0d7353637499badc2c39f799708e1ccc92
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-07-24 14:43:00 +0530

    static function

--- chapter10/01_class.py
+++ chapter10/01_class.py
@@ -6,13 +6,18 @@ class Student:
     def __init__(self,name,marks):
         self.name = name
         self.marks = marks
-        
+
+    @staticmethod
+    def hello():
+        print("Hello")   
+
     def getavg(self):
         sum = 0
         for val in self.marks:
             sum += val
-        print("Hi", self.name, "Your avg score is: ", sum/3)
+        print("Hi", self.name, "Your avg score is: ", sum/3 )
 
 s1 = Student("Tony stark", [99,98,97])
 s1.getavg()
+s1.hello()
 # print(s1.name,m1,m2,m3)
