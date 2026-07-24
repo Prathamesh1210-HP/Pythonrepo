b2f1b55997805fae5b0d3e9ff7826076289aabdd
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-07-24 14:27:27 +0530

    Class,constructor init

--- chapter10/01_class.py
+++ chapter10/01_class.py
@@ -1,17 +1,16 @@
 class Student:
+    college_name = "ABC College";
     name= "Karan";
     print("Adding new student in Database..");
-
-    # Default constructor
-    def __init__(self):
-        pass
 
     # Parameterized constructors
     def __init__(self, name,marks):
         self.name = name;
         self.marks = marks;
 
+    def welcome(self):
+        print("Welcome student,",self.name)
+
 s1 = Student("Arjun",98)
-print(s1.name,s1.marks)
-s2=Student("Karna",90)
+s1.welcome()
 print(s1.name,s1.marks)
