651567143fc6511a8657ba4cab7b1db8bc4f5590
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-15 12:41:41 +0530

    practice

--- Patterns/FloydsTriangle.py
+++ Patterns/FloydsTriangle.py
@@ -7,3 +7,16 @@ for i in range(1, n+1):
         print(num, end=" ")
         num += 1
     print()
+
+print("\n")
+
+# Practice
+
+n = 4
+
+num = 1
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(num, end=" ")
+        num = num + 1
+    print()
