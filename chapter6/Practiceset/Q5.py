dbecab9814012ddd6562691e5c8311f82b15f21f
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-20 15:09:43 +0530

    Practice

--- chapter6/Practiceset/Q5.py
+++ chapter6/Practiceset/Q5.py
@@ -1,10 +1,46 @@
 # Write a program which finds out whether a given name is present in a list or not.
-l = ["Harry","Pathu","Shubham","Diya"]
+
+l = ["Patu", "Harry", "Rani", "Prakash"]
 
 name = input("Enter your name: ")
 
-if (name in l):
-    print("Your name is in the list.")
+if(name in l):
+    print("Yes you can come.")
 
 else:
-    print("Your name is not in the list.")
+    print("Name isn't in the list.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# l = ["Harry","Pathu","Shubham","Diya"]
+
+# name = input("Enter your name: ")
+
+# if (name in l):
+#     print("Your name is in the list.")
+
+# else:
+#     print("Your name is not in the list.")
