2125db90b8ab5bf89c1a857364148b60238fcf99
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-20 13:59:38 +0530

    Revision

--- chapter6/Practiceset/Q1.py
+++ chapter6/Practiceset/Q1.py
@@ -1,18 +1,57 @@
 # Write a program to find out the greatest of four numbers entered by the user
-n1 = int(input("Enter first number: "))
-n2 = int(input("Enter Second number: "))
-n3 = int(input("Enter third number: "))
-n4 = int(input("Enter fourth number: "))
+
+n1,n2,n3,n4 = 80,20,30,40;
 
 if(n1>n2 and n1>n3 and n1>n4):
-    print(n1,"is greater number")
-elif(n2>n1 and n2>n3 and n3>n4):
-    print(n2,"is greater number")
-elif(n3>n2 and n3>n1 and n3>n4):
-    print(n1,"is greater number")
+    print(f"{n1} is greater.")
+elif(n2>n1 and n2>n3 and n1>n4):
+    print(f"{n2} is greater.")
+elif(n3>n2 and n3>n2 and n3>n4):
+    print(f"{n3} is greater.")
 elif(n4>n2 and n4>n3 and n4>n1):
-    print(n1,"is greater number")
-
-
+    print(f"{n4} is greater.")
 else:
-    print("Invalid choice")
+    print("Invalid number.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter Second number: "))
+# n3 = int(input("Enter third number: "))
+# n4 = int(input("Enter fourth number: "))
+
+# if(n1>n2 and n1>n3 and n1>n4):
+#     print(n1,"is greater number")
+# elif(n2>n1 and n2>n3 and n3>n4):
+#     print(n2,"is greater number")
+# elif(n3>n2 and n3>n1 and n3>n4):
+#     print(n1,"is greater number")
+# elif(n4>n2 and n4>n3 and n4>n1):
+#     print(n1,"is greater number")
+
+
+# else:
+#     print("Invalid choice")
