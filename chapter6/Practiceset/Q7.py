153c5afce689c9d6744e0ab10d542e598cade157
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-20 15:13:05 +0530

    Practice

--- chapter6/Practiceset/Q7.py
+++ chapter6/Practiceset/Q7.py
@@ -1,7 +1,29 @@
 # Write a program to find out whether a given post is talking about "Harry" or not
-post = "Heyy harry is good, harry is very smart"
+post = "patu is a good boy, he is an excellent developer."
 
-if("Harry".lower() in post.lower()): # lower() helps to convert post in lower form to find Harry even without capitalize form 
-    print("This post is talking about Harry")
+if("Patu".lower() in post.lower()):
+    print("This post is talking about Patu")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# post = "Heyy harry is good, harry is very smart"
+
+# if("Harry".lower() in post.lower()): # lower() helps to convert post in lower form to find Harry even without capitalize form 
+#     print("This post is talking about Harry")
 
 # "Harry" and "harry" both are different because of case sensitiveness 
