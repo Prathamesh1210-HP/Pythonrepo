dec9ff7c7f157d4688f4033f1c1ad34a00ed045b
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-22 12:37:43 +0530

    Practice

--- chapter7/Practiceset/Q2.py
+++ chapter7/Practiceset/Q2.py
@@ -1,6 +1,12 @@
-# Write a program to greet all the person names stored in  a list 'l' and which starts with S.
-l = ["Harry", "Soham", "Sachin", "Rahul"]
+# # Write a program to greet all the person names stored in  a list 'l' and which starts with S.
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
 
-for name in l:
-    if(name.startswith("S")):
-        print(f"Hello {name}")
+# for name in l:
+#     if(name.startswith("S")):
+#         print(f"Hello {name}")
+
+l = ["Sakshi", "Pratham", "Soham", "Sagar", "Pranav"]
+
+for i in l:
+    if (i.startswith("S")):
+        print(f"Hello {i}")
