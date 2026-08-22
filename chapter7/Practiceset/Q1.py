dc5cb6eb54fffcc51f1b4d2dc5cd193de2b596f1
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-22 12:32:05 +0530

    Practice

--- chapter7/Practiceset/Q1.py
+++ chapter7/Practiceset/Q1.py
@@ -1,5 +1,12 @@
 # Write a program to print multiplication table of given number using for loop
 
+# n = int(input("Enter a number: "))
+
+# i = 1
+
+# for i in range(1,11):
+#     print(f"{n} X {i} = {n*i}")
+
 n = int(input("Enter a number: "))
 
 i = 1
