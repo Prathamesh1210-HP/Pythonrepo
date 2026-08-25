7ff7d15856ffa968b835e75540c16d535d2695e9
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-25 12:10:23 +0530

    function with arg practice

--- chapter8/03_Function_with_arguments.py
+++ chapter8/03_Function_with_arguments.py
@@ -1,10 +1,18 @@
+# def goodday(name, ending):
+#     print("Good Day, " + name)
+#     print(ending)
+#     return "Done" # It will asiign to a
+
+
+# a = goodday("Pathu!", "Thank you")
+# print(a)
+# # goodday("Rutu!", "Thanks")
+# # goodday("Patu!", "Thank you")
+
 def goodday(name, ending):
-    print("Good Day, " + name)
+    print("Good day " + name)
     print(ending)
-    return "Done" # It will asiign to a
+    return "Done" #It will assign to a
 
-
-a = goodday("Pathu!", "Thank you")
+a = goodday("Pathu", "Thank you!")
 print(a)
-# goodday("Rutu!", "Thanks")
-# goodday("Patu!", "Thank you")
