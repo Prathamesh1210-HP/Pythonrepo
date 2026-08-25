09c5b8167423c494daaaea0463eaca3875b302e0
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-25 11:57:54 +0530

    Practice

--- chapter8/01_functions.py
+++ chapter8/01_functions.py
@@ -5,15 +5,15 @@
 # avg = (a + b + c)/3
 # print(avg)
 
-def avg(): # Function Definition
-        a = int(input("Enter your number: "))
-        b = int(input("Enter your number: "))
-        c = int(input("Enter your number: "))
-        avg = (a + b + c)/3
-        print(avg)
-
-avg() # Function Call
-avg()
-avg()
-avg() 
+def avg():
+    a = int(input("Enter 1st number: "))
+    b = int(input("Enter 2nd number: "))
+    c = int(input("Enter 3rd number: "))
+    avg = (a + b + c)/3
+    print(avg)
 # functions helps to do repititive tasks and reduces code size
+
+avg()
+avg()
+avg()
+avg()
