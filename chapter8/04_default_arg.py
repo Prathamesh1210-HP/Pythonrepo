44a73404f9575356957ad9efbaee221953c46848
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-25 12:16:23 +0530

    default argument

--- chapter8/04_default_arg.py
+++ chapter8/04_default_arg.py
@@ -1,7 +1,14 @@
-def goodday(name, ending = "Thank you!"):
-    print(f"Good Day, {name}")
-    print(ending)
+# def goodday(name, ending = "Thank you!"):
+#     print(f"Good Day, {name}")
+#     print(ending)
     
 
-goodday("Pathu!", "Thanks") # Here Thanks is ending value
-goodday("Rohan")  # Here there's no ending value so by default, it will take "Thank you" as default value
+# goodday("Pathu!", "Thanks") # Here Thanks is ending value
+# goodday("Rohan")  # Here there's no ending value so by default, it will take "Thank you" as default value
+
+def goodday(name, ending="Thank you!"):
+    print(f"Good Day, {name}")
+    print(ending)
+
+goodday("Patu " "Thanks!")
+goodday("Pratham")
