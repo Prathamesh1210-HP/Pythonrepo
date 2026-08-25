10cff062a482f48f7493a4e90596504177e8d0ba
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-25 12:26:20 +0530

    Recursion

--- chapter8/05_Recursion.py
+++ chapter8/05_Recursion.py
@@ -1,7 +1,15 @@
+# def facto(n):
+#     if(n==1 or n==0):
+#         return 1
+#     return n * facto(n-1)
+
+# n = int(input("Enter a number: "))
+# print(f"The factorial of this number is: {facto(n)}")
+
 def facto(n):
-    if(n==1 or n==0):
-        return 1
-    return n * facto(n-1)
+    if (n==1 or n==0):
+        return 1;
+    return n* facto(n-1);
 
 n = int(input("Enter a number: "))
 print(f"The factorial of this number is: {facto(n)}")
