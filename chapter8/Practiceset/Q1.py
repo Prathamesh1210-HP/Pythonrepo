ae6bcd08f619dd12d32bc2eace2216a44f12d1da
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-26 19:56:46 +0530

    Practice

--- chapter8/Practiceset/Q1.py
+++ chapter8/Practiceset/Q1.py
@@ -1,14 +1,28 @@
 # Write a program using functions to find ggreatest of three numbers.
+# def greatest(a, b, c):
+#     if(a>b and a>c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     else:
+#         return c
+    
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+    
+# print(greatest(a, b, c))
+
 def greatest(a, b, c):
     if(a>b and a>c):
-        return a
+        return a;
     elif(b>a and b>c):
-        return b
+        return b;
     else:
-        return c
-    
-a = int(input("Enter your number: "))
-b = int(input("Enter your number: "))
-c = int(input("Enter your number: "))
-    
-print(greatest(a, b, c))
+        return c;
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+c = int(input("Enter 3rd number: "))
+
+print(greatest(a,b,c))
