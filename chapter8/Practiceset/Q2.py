522ff92b357ab66182961471dabc734408449516
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-26 20:02:26 +0530

    Practice

--- chapter8/Practiceset/Q2.py
+++ chapter8/Practiceset/Q2.py
@@ -1,11 +1,15 @@
 # Write a python program using function to convert Celsius to Farenheit.
 # Formula: c/5 = (f-32)/9
 
+# def f_to_c(f):
+#     return 5*(f-32)/9    
+    
+# f = int(input("Enter the temperature in F: "))
+# print(f"{f_to_c(f)} °C.")
+    
 def f_to_c(f):
-    return 5*(f-32)/9    
-    
-f = int(input("Enter the temperature in F: "))
-print(f"{f_to_c(f)} °C.")
-    
+    return 5*(f-32)/9
 
+f = float(input("Enter the temperature in Farenheit: "))
 
+print(f"Temperature after converting = {f_to_c(f)} °C")
