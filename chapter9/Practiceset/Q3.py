4af72f3843843e512215456960a00a8f71dc7cef
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-09-02 16:35:30 +0530

    Practice

--- chapter9/Practiceset/Q3.py
+++ chapter9/Practiceset/Q3.py
@@ -1,4 +1,4 @@
-
+# It will print tables in mentioned numbers as well as it will create number of files as per tables.
 def generateTable(n):
     table = ""
     for i in range(1,11):
@@ -6,13 +6,6 @@ def generateTable(n):
 
     with open(f"tables/table_{n}.txt", "w") as f:
           f.write(table)
-
-    
-    
-
-
-
-
 
 
 for i in range(2,21):
