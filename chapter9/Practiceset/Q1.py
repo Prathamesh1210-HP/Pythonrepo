c298314d8d215325564bff1fc76f49bc8396f463
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-09-02 16:08:29 +0530

    Practice

--- chapter9/Practiceset/Q1.py
+++ chapter9/Practiceset/Q1.py
@@ -1,9 +1,17 @@
 # WAP to read the text from a given file 'poems.txt and find out whether it contains the word 'twinkle'.
-f = open("poem.txt")
-content = f.read()
+# f = open("poem.txt")
+# content = f.read()
 
-if("twinkle" in content):
-    print("Twinkle is present in content.")
+# if("twinkle" in content):
+#     print("Twinkle is present in content.")
+# else:
+#     print("Twinkle is not present.")
+# f.close()
+
+d = open("file.txt")
+content = d.read()
+
+if("Patu" in content):
+    print("Patu is in content.")
 else:
-    print("Twinkle is not present.")
-f.close()
+    print("Patu is not in content.")
